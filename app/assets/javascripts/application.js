34cea7b8ddc36d7f9075aff4cb1c33d07093c0c4
lrjbrual <lrjbrual@live.com>
2016-08-29 16:52:01 +0200

    Adding design layout and javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,4 +8,25 @@ $(document).ready(function(){
   $(".button-collapse").sideNav();
   $('.scrollspy').scrollSpy();
   $('.parallax').parallax();
+    $('.slider').slider({full_width: true});
+  //for btn animate the btn
+  $("btn").click(function() {
+      $('html,body').animate({
+          scrollTop: $("#services").offset().top},
+          'slow');
+  });
+});
+
+// ===== Scroll to Top ====
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
 });
