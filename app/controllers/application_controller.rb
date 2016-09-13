9e15cc7c0f92d60e5f0e61eecef9137dc219f1df
Micky0806 <mikehanot@gmail.com>
2016-09-13 13:04:29 +0200

    deleted all the tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
-def default_url_options
-  { host: ENV['HOST'] || 'localhost:3000' }
-end
-
 
 end
 
