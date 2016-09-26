37f93257b808e12a45bfb3572e787898a2ec27a1
Micky0806 <mikehanot@gmail.com>
2016-09-26 17:55:24 +0200

    add the modal to learn more

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,3 +31,8 @@ $('#return-to-top').click(function() {      // When arrow is clicked
     }, 500);
 });
 
+
+$(document).ready(function(){
+   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+   $('.modal-trigger').leanModal();
+ });
